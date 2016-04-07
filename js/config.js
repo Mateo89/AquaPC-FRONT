75c215c505f6d90d7fc274239a8c57b7c8fb1b14
Mateusz Jaskółowski <mateusz.jaskolowski@gmail.com>
2016-04-07 23:23:42 +0200

    Poprawki do formularzy oraz zmniejszenie rozmiaru plików

--- js/config.js
+++ js/config.js
@@ -1,3 +1,20 @@
+materialAdmin.config(['$httpProvider', function($httpProvider) {
+    //initialize get if not there
+    if (!$httpProvider.defaults.headers.get) {
+        $httpProvider.defaults.headers.get = {};    
+    }    
+
+    // Answer edited to include suggestions from comments
+    // because previous version of code introduced browser-related errors
+
+    //disable IE ajax request caching
+    $httpProvider.defaults.headers.get['If-Modified-Since'] = 'Mon, 26 Jul 1997 05:00:00 GMT';
+    // extra
+    $httpProvider.defaults.headers.get['Cache-Control'] = 'no-cache';
+    $httpProvider.defaults.headers.get['Pragma'] = 'no-cache';
+}]);
+
+
 materialAdmin
     .config(function ($stateProvider, $urlRouterProvider){
         $urlRouterProvider.otherwise("/home");
